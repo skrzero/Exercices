01d426ecfd3f91665c81b67c362f96d48b4812a3
skrzero <Formationafec@outlook.fr>
2025-04-23 10:25:26 +0200

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,25 @@
+let score=100;
+let energy=80;
+let hasPotion=true;
+let inventory= 'epee en bois';
+let isAlive= true;
+
+
+energy += 20;
+console.log(energy)
+console.log(hasPotion);
+energy -= 40 ;
+console.log(energy);
+score -=25;
+inventory= 'vide';
+console.log(inventory);
+energy -= 40;
+score -= 30;
+console.log(energy);
+energy -= 20;
+isAlive= false;
+
+
+
+
+console.log
