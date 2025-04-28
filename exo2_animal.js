6878d49a57d53f9c1c36eb83b97f15afc8d11c7f
skrzero <Formationafec@outlook.fr>
2025-04-28 14:40:26 +0200

    r

--- exo2_animal.js
+++ exo2_animal.js
@@ -1,12 +1,12 @@
 // number converti les chaine de caractere "" en chiffre sinon sa affiche NaN
 function revelerAnimal(magie){
-    let user =Number(prompt("choisir un chiffre entre 1 et 5"));
-        if (user >= 1 && user <= 5){
-            let animaux=["licorne","grenouille","dragon","hibou","escargot"];
-            return animaux[user -1];
+    // let user =Number(prompt("choisir un chiffre entre 1 et 5"));
+        if (magie >= 1 && magie <= 5){
+            const animaux=["licorne","grenouille","dragon","hibou","escargot"];
+            return animaux[magie -1];
         } else {
             return "Tu es trop puissant pour être classé dans notre bestiaire !";
         }
 
 }
-console.log(revelerAnimal());
+console.log(revelerAnimal(Number(prompt("choisir un chiffre entre 1 et 5"))));
