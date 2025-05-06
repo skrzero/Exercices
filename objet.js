db9e2504fecf5d9cee14dc0a28174da786b88e31
skrzero <Formationafec@outlook.fr>
2025-05-06 10:46:20 +0200

    objet

---
+++ objet.js
@@ -0,0 +1,46 @@
+let objet ={
+    prenom : "lucien",
+    age : 32,
+    ville: "dax"
+};
+console.log(objet);
+
+objet.ville = "mdm";
+objet.age =33;
+console.log(objet);
+
+objet.etudiant= true,false;
+console.log(objet);
+
+function infos(prenom,age,ville){
+console.log("je m'appelle "+ objet.prenom+" ,j'ai "+objet.age+" et j'habite a "+ objet.ville);
+}
+infos()
+
+let user =[
+    {nom :"lucien",age:33,ville:"mdm"},
+    {nom :"tom",age:23,ville:"dax"},
+    {nom :"remy",age:45,ville:"heugas"},
+]
+
+for (let i =0;i<user.length;i++)
+    console.log("nom trouver " + user[i].nom);
+
+let bibliotheque=[
+    {titre:"alpha",auteur:"beta",anneePublication:2020,disponible:true },
+    {titre:"charlie",auteur:"delta",anneePublication:2024,disponible:false},
+    {titre:"echo",auteur:"foxtrot",anneePublication:2025,disponible:true},
+]
+function emprunterLivre(titre){
+    for (let i = 0; i < bibliotheque.length; i++){
+        if (bibliotheque[i].titre === titre){
+            if (bibliotheque[i].disponible=true){
+                console.log("livre emprunté avec succés");
+            } else {
+                console.log("ce livre n'est pas disponible");
+            }
+            return;
+        }
+    }
+}
+emprunterLivre("charlie")
