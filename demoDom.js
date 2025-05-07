4575e6ed3035c5614c1e5c79e9d56990695278d4
skrzero <Formationafec@outlook.fr>
2025-05-07 15:09:39 +0200

    dom 2

--- demoDom.js
+++ demoDom.js
@@ -1,3 +1,14 @@
-function afficherDOM() {
-    console.log(document); // Montre l'objet DOM entier dans la console
-  }
+function reponseFausse(){
+  let text = document.getElementById("resultat");
+  text.textContent = "Mauvaise reponse";
+  text.style.color = "red";
+}
+function reponseBonne(){
+  let text = document.getElementById("resultat");
+  text.textContent = "Bonne reponse";
+  text.style.color = "green";
+}
+function reset(){
+let reset2 = document.getElementById("resultat");
+reset2.textContent="";
+}
