056c45f987a1e91660d4272bf878534fd284e6d6
skrzero <Formationafec@outlook.fr>
2025-05-07 16:43:24 +0200

    campexploration

--- campExploration.js
+++ campExploration.js
@@ -1,3 +1,14 @@
 function ajouterObjet(){
-    
+    const select = document.getElementById("objetSelect");
+    const item = select.value;
+        console .log(item);
+    const sac = document.getElementById("sac");
+    const itemSelect = document.createElement("div");
+    const paragraphe = document.createElement("p");
+    paragraphe.textContent= item;    
+    itemSelect.appendChild(paragraphe);
+    sac.appendChild(itemSelect);
+    const boutonSup = document.createElement("button");
+    boutonSup.textContent = "supprimer";
+    console.log(boutonSup);   
 }
