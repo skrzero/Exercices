7d845b139bdf425dccd9c85352607fdf170ffae5
skrzero <Formationafec@outlook.fr>
2025-05-09 16:30:10 +0200

    fini camps

--- campExploration.js
+++ campExploration.js
@@ -3,13 +3,34 @@ function ajouterObjet(){
     const item = select.value;
         console .log(item);
     const sac = document.getElementById("sac");
+    const camp= document.getElementById("camp");
     const itemSelect = document.createElement("div");
-    const paragraphe = document.createElement("p");
+    const paragraphe = document.createElement("span");
     paragraphe.textContent= item;    
     itemSelect.appendChild(paragraphe);
+    // paragraphe dans la div 
     sac.appendChild(itemSelect);
     const boutonSup = document.createElement("button");
-    boutonSup.textContent = "supprimer";
-    sac.appendChild(boutonSup);
-    console.log(boutonSup);   
+    boutonSup.textContent = "Supprimer";
+    itemSelect.appendChild(boutonSup);
+    console.log(boutonSup);
+    const boutonMove =document.createElement("button");
+    boutonMove.textContent = "Déplacer";
+    itemSelect.appendChild(boutonMove);
+    boutonSup.style.marginLeft= "10px";
+    boutonSup.onclick = function(){
+        // sac.removeChild(itemSelect);
+        itemSelect.remove();
+        console.log("supp");
+    };
+    boutonMove.onclick = function(){
+        const parentId= itemSelect.parentElement.id;
+        if (parentId==="sac"){
+          camp.appendChild(itemSelect);
+        } else {
+          sac.appendChild(itemSelect);  
+        } 
+        
+        console.log("cpt");
+    }
 }
