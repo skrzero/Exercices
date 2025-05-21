359c12b5a93e4dc8c9d3ae77ba662e6ab4daf791
skrzero <Formationafec@outlook.fr>
2025-05-21 16:12:14 +0200

    personnage rick et morty

--- js/personnageRick&Morty.js
+++ js/personnageRick&Morty.js
@@ -1,5 +1,5 @@
 // On récupère le nombre total de personnages
-let totalPersonnages;
+let totalPersonnages = 0;
 
 async function initialiserNombreDePersonnages() {
   let response = await fetch("https://rickandmortyapi.com/api/character");
@@ -13,8 +13,30 @@ initialiserNombreDePersonnages();
 let bouton = document.getElementById("btn-rm");
 let zoneAffichage = document.getElementById("character");
 
-
 // On écoute le clic sur le bouton
-bouton.addEventListener("click", function () {
-  // TON CODE ICI !
+bouton.addEventListener("click", async function () {
+  if (totalPersonnages > 0) {
+    try {
+      let random = Math.floor(Math.random() * totalPersonnages) + 1;
+      console.log("test");
+      let url = `https://rickandmortyapi.com/api/character/${random}`;
+      let response = await fetch(url);
+      let data = await response.json();
+      let soloPersonnage = {
+        name: data.name,
+        species: data.species,
+        status: data.status,
+        image: data.image,
+      };
+      console.log(soloPersonnage);
+      zoneAffichage.innerHTML = `
+  <h2>${soloPersonnage.name}</h2>
+  <p>Espèce : ${soloPersonnage.species}</p>
+  <p>Status : ${soloPersonnage.status}</p>
+  <img src="${soloPersonnage.image}" alt="${soloPersonnage.name}" />
+`;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 });
