2dc9b5aba59a25b92dc489d756eaac67000fb955
skrzero <Formationafec@outlook.fr>
2025-05-23 17:15:31 +0200

    local storage reste a faire revenir le tableau apres le reset

--- js/localStorageExo.js
+++ js/localStorageExo.js
@@ -8,11 +8,58 @@ function ajouterPrenom() {
   li.id = id;
   id++;
   let liste = ul.appendChild(li);
-
-
+  prenom.value = "";
+  sauvegardePrenom();
 }
-ajouterPrenom();
+
 
 function supprimerPrenom() {
+  let trouver = false;
+  let itemLi = ul.getElementsByTagName("li"); /* recupere tout les element "li" de ul*/
+  for (let li of itemLi) {
+    if (li.textContent === prenom.value) {
+      li.remove();
+      trouver = true;
+      prenom.value = "";
+      sauvegardePrenom();
+      return;
+    }
+  }
+  if (!trouver) {
+    /* si la boucle na pas trouver = false l'alert ce lance sinon il ne fait rien */
+    alert("prenom inexistant");
+  }
 
+  let donnees = localStorage.getItem("listePrenoms");
+  if (donnees !== null) {
+    let prenoms = JSON.parse(donnees);
+    
+  }
 }
+
+
+function sauvegardePrenom() {
+  let prenoms = [];
+  let itemLi = ul.getElementsByTagName("li");
+  for (let li of itemLi) {
+    prenoms.push(li.textContent);
+  }
+  let tableau = JSON.stringify(prenoms);
+  localStorage.setItem("listePrenoms", tableau);
+  console.log(prenoms);
+}
+sauvegardePrenom();
+
+window.onload = function () {
+  ul.innerHTML = ""; // vide la liste avant de la remplir
+  let donnees = localStorage.getItem("listePrenoms");
+  if (donnees !== null) {
+    let prenoms = JSON.parse(donnees);
+    for (let p of prenoms) {
+      let li = document.createElement("li");
+      li.textContent = p;
+      ul.appendChild(li);
+    }
+  }
+};
+
