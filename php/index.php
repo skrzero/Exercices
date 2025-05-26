cf6c9bd651f17a04f107c99075d04ecdcda81d9f
skrzero <Formationafec@outlook.fr>
2025-05-26 11:36:45 +0200

    settings php

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<?php
+include ("settings.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href= "<?php echo CSS_DIR;?>" /> 
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="titre">test</h1>
+</body>
+</html>
---
+++ php/settings.php
@@ -0,0 +1,3 @@
+<?php
+define ('CSS_DIR','style2.css');
+?>
